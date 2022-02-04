544d09921d277a79aab979d539b04838cc8c90cd
Александра Новоселова <alexandra-novoselova@outlook.com>
2022-02-05 00:07:11 +1000

    Добавьте файлы проекта.

---
+++ HomeworkFive/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeworkFive
+{
+    class Program
+    {
+        static void PrintText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static string[] SplitText (string text)
+        {
+            string[] splitText = text.Split(' ');
+            return splitText;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputText = Console.ReadLine();
+            string[] lines = SplitText(inputText);
+            Console.WriteLine("Вывод приложения:");
+            foreach (string element in lines)
+            {
+                PrintText(element);
+            }
+
+            
+
+        }
+    }
+}
---
+++ HomeworkFive_TaskTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeworkFive_TaskTwo
+{
+    class Program
+    {
+        static string[] ReversWords(string inputPhrase)
+        {
+            string[] splitedText = SplitText(inputPhrase);
+            Array.Reverse(splitedText);
+            string[] reversedText = splitedText;
+            return reversedText;
+        }
+        static string[] SplitText(string inputPhrase)
+        {
+            string[] splitedText = inputPhrase.Split(' ');
+            return splitedText;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputText = Console.ReadLine();
+            string[] newText = ReversWords(inputText);
+            Console.WriteLine("Перевернутое предложение:");
+            foreach (string element in newText) 
+            {
+                Console.Write(element + " ");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
